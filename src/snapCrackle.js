20a2a4219759b7b00f20f1ac9ec8a3470ab03346
GabrielBraganca <gabriel.cardozo2804@gmail.com>
2021-10-18 23:04:29 -0300

    entrega trabalho 19/10

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,32 @@
 // Seu código vai aqui
+
+
+
+ function snapCrackle (maxValue){
+    let texto = "__";
+
+for (let counter = 1; counter <= maxValue; counter++){
+   
+    if (counter % 5 == 0 && counter % 2 !=0){ 
+        texto = texto + " SnapCrackle ";
+    } else if( counter % 2 != 0){
+        texto = texto + " Snap ";
+    } else if(counter % 5 == 0){
+        texto = texto + " Crackle";
+    } else {texto = texto + counter}
+    
+    if(maxValue != counter){ 
+        texto = texto + ",";
+    } else {
+        texto = texto + "__";
+    }
+}
+
+return texto
+}
+
+console.log(snapCrackle(12)) 
+
+
+
+
