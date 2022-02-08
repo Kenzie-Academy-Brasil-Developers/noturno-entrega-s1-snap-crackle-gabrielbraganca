120a2b33cdee8bc7da5f3ba54cb36e498f4a1592
unknown <gabriel.cardozo2804@gmail.com>
2022-02-08 20:36:51 -0300

    correcao

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -8,22 +8,22 @@
 for (let counter = 1; counter <= maxValue; counter++){
    
     if (counter % 5 == 0 && counter % 2 !=0){ 
-        texto = texto + " SnapCrackle ";
+        texto = texto + "SnapCrackle";
     } else if( counter % 2 != 0){
-        texto = texto + " Snap ";
+        texto = texto + "Snap";
     } else if(counter % 5 == 0){
-        texto = texto + " Crackle";
+        texto = texto + "Crackle";
     } else {texto = texto + counter}
     
     if(maxValue != counter){ 
-        texto = texto + ",";
+        texto = texto + ", ";
     } ;
 }
 
 return texto
 }
 
-console.log(snapCrackle(15)) 
+console.log(snapCrackle(12)); 
 
 
 
